0a02ad19cfd8aeff386154fef83cd48190955c11
Ignat <ai73a@yandex.ru>
2019-12-20 17:31:38 +0300

    Updates

---
+++ precache-manifest.878c7ebe4c465722254cade9ba98937b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2f25586fd443ff4f1c472a6811269d",
+    "url": "/boolean-reducer-modal-time-color/index.html"
+  },
+  {
+    "revision": "1e59962d203441bd6056",
+    "url": "/boolean-reducer-modal-time-color/static/css/2.826c19ff.chunk.css"
+  },
+  {
+    "revision": "aba9a98751a79ffb0055",
+    "url": "/boolean-reducer-modal-time-color/static/css/main.ad277072.chunk.css"
+  },
+  {
+    "revision": "1e59962d203441bd6056",
+    "url": "/boolean-reducer-modal-time-color/static/js/2.4387eef6.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/boolean-reducer-modal-time-color/static/js/2.4387eef6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "aba9a98751a79ffb0055",
+    "url": "/boolean-reducer-modal-time-color/static/js/main.21e79ae4.chunk.js"
+  },
+  {
+    "revision": "82a173851bc96f20e13a",
+    "url": "/boolean-reducer-modal-time-color/static/js/runtime-main.d44f64be.js"
+  }
+]);
